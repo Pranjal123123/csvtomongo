c70fd348a144e6baf5b1bcd3c9038d49ca4eabb2
pranjaltechnical <pranjaltech.com>
2022-12-16 02:40:35 +0530

    remaining done

--- routes/resource.js
+++ routes/resource.js
@@ -8,6 +8,8 @@ var csvmodels = require('../src/models/excldata');
 const path = require("path");
 const methodOverride = require("method-override");
 var async = require('async');
+const reorder = require('csv-reorder');
+
 
 router.use(methodOverride("_method"));
 router.use(bodyParser.json());
@@ -51,7 +53,7 @@ router.post('/post', upload.single('csv'), async(req, res) => {
     .then((alldata) => {
       //console.log(alldata);
 
-
+      // use of async.eachseries
       async.eachSeries(alldata,function(item, cb){
         setTimeout(function() {
           console.log(item);
@@ -61,24 +63,89 @@ router.post('/post', upload.single('csv'), async(req, res) => {
         console.log(err);
       });
 
+      //method 1
+      // const unique = new Set(alldata);
 
-      csvmodels.insertMany(alldata,(err, data) => {
-        if (err) {
-          req.flash('server-error',"Something wrong");
-          res.redirect('/');
-          console.log(err);
-        } else {
-          req.flash('server-success',"Thank You.Your File has been Successfully uploaded");
-          res.redirect('/');
-          // res.send("successfully send");
-        }
+      //method 2
+      //reorder package
+      reorder({
+        input: req.file.path,
+        output: './public/csv/output.csv',
+        sort: 'Email',
+        type: 'string',
+        remove: true,
+        "remove-duplicates": true,
+        metadata: false
+      })
+      .then((metadata) => {
+
+        console.log(metadata);
+
+        //csv of output
+        csv().fromFile('./public/csv/output.csv')
+        .then((productdata)=>{
+        csvmodels.insertMany(productdata,(err, data) => {
+          if (err) {
+            req.flash('server-error',"Something wrong");
+            res.redirect('/');
+            console.log(err);
+          } else {
+            req.flash('server-success',"Thank You.Your File has been Successfully uploaded");
+            res.redirect('/');
+            // res.send("successfully send");
+          }
+        });
+        })
+        .catch(error => {
+          console.log(error)
+        });  
+        // output of csv ends
+      })
+      .catch(error => {
+        console.log(error)
       });
+     // reorder package ends
+
     });
   }
   catch{
     console.log(err);
     res.send("There is a possibility of error");   
   }
- 
 });
+
+// method 3 (but not able to implement)
+  // router.post("/post",upload.single('csv'), async (req, res) => {
+  //   const products = await csv().fromFile(req.file.path);
+    
+  //   try {
+  //     products.map(async (pdata) => {
+  //       let uniqueCode= await csvmodels.findOne({
+  //         Email: pdata.Email,
+  //       });
+  //       if (!uniqueCode) {
+  //         //create new object
+  //         let product = new csvmodels({
+  //           Name:pdata.Name,
+  //           Email:pdata.Email,
+  //           MobileNumber:pdata.MobileNumber,
+  //           DateOfBirth:pdata.DateOfBirth,
+  //           WorkExperience:pdata.WorkExperience,
+  //           ResumeTitle:pdata.ResumeTitle,
+  //           CurrentLocation:pdata.CurrentLocation,
+  //           PostalAddress:pdata.PostalAddress,
+  //           CurrentEmployer:pdata.CurrentEmployer,
+  //           CurrentDesignation:pdata.CurrentDesignation,
+  //         });
+  //          await product.save();
+  //       }
+  //     });
+  //     req.flash('server-success',"Thank You.Your File has been Successfully uploaded");
+  //     res.redirect('/');
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // })
+ 
+
 module.exports = router;
--- src/models/excldata.js
+++ src/models/excldata.js
@@ -4,7 +4,7 @@ const { ObjectId } = mongoose.Schema.Types;
 const resourceSchema = new mongoose.Schema({
     Name:{
         type: String
-    },
+    }, 
     Email:{
         type: String
     },
